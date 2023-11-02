4d313f249230c3cfbf15531af2f18c27aa9cb3ca
careyxi <careyxi@zego.im>
2023-11-02 10:34:39 +0800

    clear appid.

---
+++ best_practice/ZegoCloudSDKDemo/KeyCenter.swift
@@ -0,0 +1,11 @@
+//
+//  KeyCenter.swift
+//  ZegoCloudSDKDemo
+//
+//  Created by zego on 2023/11/2.
+//
+
+import Foundation
+
+let APP_ID: UInt32 = <#YourAppID#>
+let APP_SIGN: String = <#YourAppSign#>
